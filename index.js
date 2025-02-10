77835d342589f7cef21f435c62cec2d7a0942b61
PetroYar <petryarish@gmail.com>
2025-02-10 16:16:11 +0200

    test

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { createServer } from "node:http";
 
+
+const data = {
+  users: [
+    {
+      name: "vasa",
+    },
+    { name: "petya" },
+  ],
+};
+
 const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("He World!\n");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(data));
 });
 
 server.listen(3000, "127.0.0.1", () => {
